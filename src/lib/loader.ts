924c84bae2f842e248345d323c07d7cb6455588b
Iain Calder <iain.calder@gmail.com>
2024-06-07 18:22:29 +0100

    feat: basic style filtering

--- src/lib/loader.ts
+++ src/lib/loader.ts
@@ -1,13 +1,13 @@
 import { DigitalBoard } from "@/untappd/digitalboard";
-//import { dataBoardId } from "./dataBoardId";
-import data from "../../digital_board.json";
+import { dataBoardId } from "./dataBoardId";
+//import data from "../../digital_board.json";
 
 export async function loader() {
-  //const { boardId } = dataBoardId();
-  // const response = await fetch(
-  //   `https://business.untappd.com/api/v1/digital_boards/${boardId}?public=true`
-  //   // "https://business.untappd.com/api/v1/digital_boards/20462?public=true"
-  // );
-  //const data = await response.json();
+  const { boardId } = dataBoardId();
+  const response = await fetch(
+    `https://business.untappd.com/api/v1/digital_boards/${boardId}?public=true`
+    // "https://business.untappd.com/api/v1/digital_boards/20462?public=true"
+  );
+  const data = await response.json();
   return data.digital_board as unknown as DigitalBoard;
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import {
   Select,
+  SelectGroup,
   SelectItem,
   SelectTrigger,
   SelectValue,
@@ -9,9 +10,13 @@ import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { loader } from "@/lib/loader";
 import { SelectContent } from "@radix-ui/react-select";
+import React from "react";
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 export default function Home() {
+  const [chosenStyle, setChosenStyle] = useState("_all_");
+
   const board = useLoaderData() as Awaited<ReturnType<typeof loader>>;
   const menu = board.menus[0];
   const sections = menu.sections;
@@ -20,12 +25,17 @@ export default function Home() {
     if (!section.name) {
       section.name = "Beers";
     }
-    // Fill in section style options
+    // Set section style options
     section.styles = [...new Set(section.items.map((item) => item.style))];
-    console.log(section.styles);
   }
 
-  // https://stackoverflow.com/questions/78321710/shadcn-ui-select-change-state-when-user-selects-item
+  const handleTabChanged = () => {
+    setChosenStyle("_all_");
+  };
+
+  const handleStyleSelected = (style: string) => {
+    setChosenStyle(style);
+  };
 
   return (
     <>
@@ -33,62 +43,77 @@ export default function Home() {
       <h2>{board.website}</h2>
       <h3>{menu.name}</h3>
       <Separator className="my-4" />
-      <Tabs defaultValue={sections[0].name} className="w-[400px]">
+      <Tabs
+        defaultValue={sections[0].name}
+        onValueChange={handleTabChanged}
+        className="w-[400px]"
+      >
         <TabsList>
           {sections.map((section) => (
-            <TabsTrigger value={section.name}>{section.name}</TabsTrigger>
+            <TabsTrigger key={section.id} value={section.name}>
+              {section.name}
+            </TabsTrigger>
           ))}
         </TabsList>
         {sections.map((section) => (
-          <TabsContent value={section.name}>
-            <Select>
+          <TabsContent key={section.id} value={section.name}>
+            <Select onValueChange={handleStyleSelected}>
               <SelectTrigger className="w-[280px]">
                 <SelectValue placeholder="Choose style" />
               </SelectTrigger>
               <SelectContent>
-                {section.styles.map((style) => (
-                  <SelectItem
-                    className="bg-slate-950"
-                    key="style"
-                    value={style}
-                  >
-                    {style}
-                  </SelectItem>
-                ))}
+                <SelectGroup>
+                  <SelectItem value="_all_">All</SelectItem>
+                </SelectGroup>
+                <SelectGroup className="overflow-y-auto max-h-[20rem]">
+                  {section.styles.map((style) => (
+                    <SelectItem
+                      className="bg-slate-950"
+                      key={style}
+                      value={style}
+                    >
+                      {style}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
               </SelectContent>
             </Select>
             <Separator className="my-4" />
-            {section.items.map((item) => (
-              <Card>
-                <CardTitle>
-                  <span className="font-bold">{item.brewery}</span>&nbsp;
-                  <span className="font-normal">{item.name}</span>
-                </CardTitle>
-                <CardContent className="flex flex-row items-start gap-2 mt-1">
-                  <img
-                    src={item.label_image_thumb}
-                    height="50"
-                    width="50"
-                    alt="label image"
-                  ></img>
-                  <div>
-                    <p>{item.style}</p>
-                    <p>{item.abv}% ABV</p>
-                    <p>{item.brewery_location}</p>
-                    <p>
-                      {item.containers.map((container) => (
-                        <>
-                          <span className="mr-2">{container.name}</span>
-                          <span className="font-bold mr-2">
-                            £{container.price}
-                          </span>
-                        </>
-                      ))}
-                    </p>
-                  </div>
-                </CardContent>
-              </Card>
-            ))}
+            {section.items
+              .filter(
+                (item) => chosenStyle == "_all_" || item.style == chosenStyle
+              )
+              .map((item) => (
+                <Card key={item.id}>
+                  <CardTitle>
+                    <span className="font-bold">{item.brewery}</span>&nbsp;
+                    <span className="font-normal">{item.name}</span>
+                  </CardTitle>
+                  <CardContent className="flex flex-row items-start gap-2 mt-1">
+                    <img
+                      src={item.label_image_thumb}
+                      height="50"
+                      width="50"
+                      alt="label image"
+                    ></img>
+                    <div>
+                      <p>{item.style}</p>
+                      <p>{item.abv}% ABV</p>
+                      <p>{item.brewery_location}</p>
+                      <p>
+                        {item.containers.map((container) => (
+                          <React.Fragment key={container.name}>
+                            <span className="mr-2">{container.name}</span>
+                            <span className="font-bold mr-2">
+                              £{container.price}
+                            </span>
+                          </React.Fragment>
+                        ))}
+                      </p>
+                    </div>
+                  </CardContent>
+                </Card>
+              ))}
           </TabsContent>
         ))}
       </Tabs>
