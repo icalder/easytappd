c33fe4dfbb2db364bdc152ac3e8751a8c7319c52
Iain Calder <iain.calder@gmail.com>
2024-06-07 19:08:07 +0100

    chore: convert to publish action for GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      // dev specific config
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+    };
+  } else {
+    // command === 'build'
+    return {
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+      base: "/easytappd/",
+    };
+  }
 });
